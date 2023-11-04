6feb5f4a39527275778071145ae0660806f5a936
Gabriel <3950465+gspalato@users.noreply.github.com>
2023-11-04 18:26:53 -0300

    Started migrating Frontier to Next.JS with Nextra.

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Page from '@components/Page';
+
+const Component = () => {
+	return (
+		<Page>
+			<section className='transition-250 flex h-full w-full items-center justify-center ease-in-out'>
+				<h1 style={{ fontFamily: 'Space Grotesk', fontSize: '10rem' }}>
+					404
+				</h1>
+			</section>
+		</Page>
+	);
+};
+
+export default Component;
---
+++ theme.config.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import FoundationLogo from '@assets/img/FoundationIcon.svg';
+
+export default {
+    head: (
+        <>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+            <meta name="theme-color" content="#000000" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta
+                name="apple-mobile-web-app-status-bar-style"
+                content="black-translucent"
+            />
+
+            <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+            <link rel="manifest" href="/manifest.json">
+
+            <title>Foundation</title>
+        </>
+    ),
+}
